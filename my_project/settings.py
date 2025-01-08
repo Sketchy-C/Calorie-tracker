5bc743578072f0e55f09827fb0f9de29e1a79b11
Sketchy-C <willy254dabs@gmail.com>
2025-01-08 00:30:07 -0800

    new

--- my_project/settings.py
+++ my_project/settings.py
@@ -78,17 +78,20 @@ WSGI_APPLICATION = 'my_project.wsgi.application'
 # Database
 # https://docs.djangoproject.com/en/5.1/ref/settings/#databases
 
-DATABASES = {
-    'default': {
-        'ENGINE': 'django.db.backends.postgresql',
-        'NAME': 'calorie',
-        'USER': 'willy254',
-        'PASSWORD': '123456',
-        'HOST': 'localhost',
-        'PORT':'5432',
-    }
-}
+# DATABASES = {
+#     'default': {
+#         'ENGINE': 'django.db.backends.postgresql',
+#         'NAME': 'calorie',
+#         'USER': 'willy254',
+#         'PASSWORD': '123456',
+#         'HOST': 'localhost',
+#         'PORT':'5432',
+#     }
+# }
 
+DATABASES={
+    'default':dj_database_url.parse("")
+}
 
 # Password validation
 # https://docs.djangoproject.com/en/5.1/ref/settings/#auth-password-validators
